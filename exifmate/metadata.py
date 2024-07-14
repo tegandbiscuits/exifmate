a12b31300dbb33142d4242e15a8c53a6bc8f4286
Tegan Rauh <onlysayinghi@gmail.com>
2024-07-14 00:54:05 -0400

    Lint

--- exifmate/metadata.py
+++ exifmate/metadata.py
@@ -1,5 +1,6 @@
 import json
-from pathlib import Path 
+from pathlib import Path
+
 from PIL import Image
 from PIL.ExifTags import IFD
 
@@ -7,8 +8,9 @@ f = Path.open("./exifmate/editable_metadata.json")
 EDITABLE_METADATA = json.load(f)
 f.close()
 
+
 class Metadata:
-  def __init__(self, image: Image):
+  def __init__(self, image: Image) -> None:
     self.image = image
     self.exif = self.image.getexif()
 
@@ -16,18 +18,13 @@ class Metadata:
     tag = EDITABLE_METADATA["exif"][tag_name]
     base_value = self.exif.get(tag["tag_id"])
 
-    if base_value == None:
+    if base_value is None:
       base_value = self.exif.get_ifd(IFD.Exif).get(tag["tag_id"])
 
     if "value_to_human" in tag:
       return tag["value_to_human"].get(base_value) or base_value
 
-    if type(base_value) == bytes:
+    if isinstance(base_value, bytes):
       return base_value.decode("utf8")
 
     return base_value
-
-  # @staticmethod
-  # def editable_metadata():
-  #   f = Path.open("./exifmate/editable_metadata.json")
-  #   return json.load(f)
--- tests/metadata_test.py
+++ tests/metadata_test.py
@@ -1,8 +1,9 @@
-from exifmate.metadata import Metadata, EDITABLE_METADATA
-from PIL.ExifTags import TAGS, IFD
-from PIL import Image
 import pytest
-import pdb;
+from PIL import Image
+from PIL.ExifTags import IFD, TAGS
+
+from exifmate.metadata import EDITABLE_METADATA, Metadata
+
 
 class TestEditableMetadata:
   def test_contains_valid_exif_tags(self):
@@ -11,11 +12,12 @@ class TestEditableMetadata:
       tag_id = EDITABLE_METADATA["exif"][tag_label]["tag_id"]
       assert TAGS[tag_id] == tag_label
       assertion_count += 1
-    
+
     assert assertion_count == len(EDITABLE_METADATA["exif"])
 
-@pytest.fixture
-def create_test_image(tmp_path):
+
+@pytest.fixture()
+def create_test_image():
   def _create_test_image(exif_data: dict) -> Image:
     img = Image.new("1", [1, 1])
     exif = img.getexif()
@@ -26,7 +28,7 @@ def create_test_image(tmp_path):
       tag_value = exif_data[tag_name]
       tag_id = EDITABLE_METADATA["exif"][tag_name]["tag_id"]
 
-      if type(tag_value) == dict and tag_value.get("in_ifd"):
+      if isinstance(tag_value, dict) and tag_value.get("in_ifd"):
         exif.get_ifd(IFD.Exif)[tag_id] = tag_value["value"]
       else:
         exif[tag_id] = tag_value
@@ -37,30 +39,34 @@ def create_test_image(tmp_path):
 
 
 class TestMetadataRead:
-  @pytest.mark.parametrize("make_value", ["Test Camera", None])
+  @pytest.mark.parametrize(("make_value"), ["Test Camera", None])
   def test_when_value_is_arbitrary(self, create_test_image, make_value):
-    test_image = create_test_image({ "Make": make_value })
+    test_image = create_test_image({"Make": make_value})
     m = Metadata(test_image)
     assert m.read("Make") == make_value, "returns raw value"
 
-  @pytest.mark.parametrize("dto_value", ["2010:01:01 00:00:00", None])
-  def test_when_tag_is_in_an_ifd(self, create_test_image, dto_value ):
-    test_image = create_test_image({ "DateTimeOriginal": { "in_ifd": True, "value": dto_value }})
+  @pytest.mark.parametrize(("dto_value"), ["2010:01:01 00:00:00", None])
+  def test_when_tag_is_in_an_ifd(self, create_test_image, dto_value):
+    test_image = create_test_image(
+      {"DateTimeOriginal": {"in_ifd": True, "value": dto_value}},
+    )
     m = Metadata(test_image)
     assert m.read("DateTimeOriginal") == dto_value, "still returns the value"
 
   # Returning unknown values as the original so it doesn't act destructive.
   # TODO: the UI needs to make sure the unknown value is a selectable option
   @pytest.mark.parametrize(
-    "wb_value,expected",
-    [("1", "Manual"), ("123", "123"), (None, None)]
+    ("wb_value", "expected"),
+    [("1", "Manual"), ("123", "123"), (None, None)],
   )
   def test_when_values_are_predefined(self, create_test_image, wb_value, expected):
-    test_image = create_test_image({ "WhiteBalance": wb_value })
+    test_image = create_test_image({"WhiteBalance": wb_value})
     m = Metadata(test_image)
     assert m.read("WhiteBalance") == expected
 
   def test_when_value_is_stored_in_bytes(self, create_test_image):
-    test_image = create_test_image({ "ExifVersion": { "in_ifd": True, "value": bytes("0232", "utf8") } })
+    test_image = create_test_image(
+      {"ExifVersion": {"in_ifd": True, "value": bytes("0232", "utf8")}},
+    )
     m = Metadata(test_image)
     assert m.read("ExifVersion") == "0232", "returns UTF-8 serialized"
