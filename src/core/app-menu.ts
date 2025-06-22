012cd8f827e8166410e29dfec1d223ffbb65467e
Tegan Rauh <onlysayinghi@gmail.com>
2025-06-21 23:06:57 -0400

    Build out menus

--- src/core/app-menu.ts
+++ src/core/app-menu.ts
@@ -24,10 +24,35 @@ export async function createAppMenu() {
         },
       },
     ],
+    // TODO: add save options
+  });
+
+  const editMenu = await Submenu.new({
+    text: 'Edit',
+    items: [
+      // should i have a note to users that this isn't more than undoing text?
+      await PredefinedMenuItem.new({ item: 'Undo' }),
+      await PredefinedMenuItem.new({ item: 'Redo' }),
+      await PredefinedMenuItem.new({ item: 'Separator' }),
+      await PredefinedMenuItem.new({ item: 'Cut' }),
+      await PredefinedMenuItem.new({ item: 'Copy' }),
+      await PredefinedMenuItem.new({ item: 'Paste' }),
+      await PredefinedMenuItem.new({ item: 'SelectAll' }),
+    ],
+  });
+
+  const viewMenu = await Submenu.new({
+    text: 'View',
+    items: [await PredefinedMenuItem.new({ item: 'Fullscreen' })],
+  });
+
+  const windowMenu = await Submenu.new({
+    text: 'Window',
+    items: [await PredefinedMenuItem.new({ item: 'Minimize' })],
   });
 
   const menu = await Menu.new({
-    items: [appMenu, fileMenu],
+    items: [appMenu, fileMenu, editMenu, viewMenu, windowMenu],
   });
 
   menu.setAsAppMenu();
