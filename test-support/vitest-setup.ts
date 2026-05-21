96e30c5323f55474c1e0cd2c7134a2e4b77e3f61
Tegan Rauh <onlysayinghi@gmail.com>
2026-05-20 22:20:42 -0400

    Resolve specs that fail with mock resetting

--- test-support/vitest-setup.ts
+++ test-support/vitest-setup.ts
@@ -7,7 +7,7 @@ window.HTMLElement.prototype.scrollIntoView = () => {};
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
-  value: vi.fn().mockImplementation((query) => ({
+  value: vi.fn((query) => ({
     matches: false,
     media: query,
     onchange: null,
